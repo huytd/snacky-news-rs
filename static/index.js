5661e7b3be06e9c668e65c59c5d6ecd5a3683f80
Huy Tran <kingbazoka@gmail.com>
2020-04-17 16:20:47 -0700

    Improve pagination

--- static/index.js
+++ static/index.js
@@ -9,9 +9,13 @@ const HALF_PAGE_ARTICLE = (article) => article.text.length >= 5000 && article.te
 const ONE_THIRD_ARTICLE = (article) => article.text.length >= 2500 && article.text.length < 5000;
 const ONE_FOURTH_ARTICLE = (article) => article.text.length < 2500;
 
-Array.prototype.take = function(n) {
+Array.prototype.take = function (n) {
     const ret = this.splice(0, n);
     return ret;
+};
+
+String.prototype.forDisplay = function () {
+    return this.replace(/(.)\.([^\d\W])/g, '$1. $2').replace(/\n/g, "\n\n");
 };
 
 const getDomainFromUrl = url => {
@@ -64,7 +68,7 @@ const copyToClipboard = (input) => {
     let success = false;
     try {
         success = document.execCommand('copy');
-    } catch (err) {}
+    } catch (err) { }
 
     document.body.removeChild(el);
 
@@ -85,14 +89,14 @@ const FullPageLayout = (props) => {
                 <h4 className="my-2">{first.title}</h4>
                 <div className="three-columns text-justify">
                     {first.image && (<div className="image-cover mb-4 overflow-hidden"><img src={first.image} /></div>)}
-                    {first.text.replace(/\n/g, "\n\n")}
+                    {first.text.forDisplay()}
                 </div>
             </div>
             <div className="article-right w-1/4 pl-5">
                 <h4 className="my-2">{second.title}</h4>
                 <div className="two-columns text-justify">
                     {second.image && (<div className="image-cover mb-4 overflow-hidden"><img src={second.image} /></div>)}
-                    {second.text.replace(/\n/g, "\n\n")}
+                    {second.text.forDisplay()}
                 </div>
             </div>
         </div>
@@ -108,24 +112,24 @@ const FullPageLayoutLeft = (props) => {
                 <h5 className="my-2">{second.title}</h5>
                 <div className="three-columns text-justify">
                     {second.image && (<div className="image-cover mb-4 overflow-hidden"><img src={second.image} /></div>)}
-                    {second.text.replace(/\n/g, "\n\n")}
+                    {second.text.forDisplay()}
                 </div>
                 <h5 className="my-2">{third.title}</h5>
                 <div className="three-columns text-justify">
                     {third.image && (<div className="image-cover mb-4 overflow-hidden"><img src={third.image} /></div>)}
-                    {third.text.replace(/\n/g, "\n\n")}
+                    {third.text.forDisplay()}
                 </div>
                 <h5 className="my-2">{fourth.title}</h5>
                 <div className="three-columns text-justify">
                     {fourth.image && (<div className="image-cover mb-4 overflow-hidden"><img src={fourth.image} /></div>)}
-                    {fourth.text.replace(/\n/g, "\n\n")}
+                    {fourth.text.forDisplay()}
                 </div>
             </div>
             <div className="article-right flex-1 pl-5">
                 <h4 className="my-2">{first.title}</h4>
                 <div className="four-columns text-justify">
                     {first.image && (<div className="image-cover mb-4 overflow-hidden"><img src={first.image} /></div>)}
-                    {first.text.replace(/\n/g, "\n\n")}
+                    {first.text.forDisplay()}
                 </div>
             </div>
         </div>
@@ -139,23 +143,23 @@ const TripleLayout = (props) => {
             <div className="six-columns">
                 {first && <>
                     <h4 className="my-2">{first.title}</h4>
-                    <div className="my-4 text-justify">
+                    <div className="my-4 text-justify break-words">
                         {first.image && (<div className="image-cover mb-4 overflow-hidden"><img src={first.image} /></div>)}
-                        {first.text.replace(/\n/g, "\n\n")}
+                        {first.text.forDisplay()}
                     </div>
                 </>}
                 {second && <>
                     <h4 className="my-2">{second.title}</h4>
-                    <div className="my-4 text-justify">
+                    <div className="my-4 text-justify break-words">
                         {second.image && (<div className="image-cover mb-4 overflow-hidden"><img src={second.image} /></div>)}
-                        {second.text.replace(/\n/g, "\n\n")}
+                        {second.text.forDisplay()}
                     </div>
                 </>}
                 {third && <>
                     <h4 className="my-2">{third.title}</h4>
-                    <div className="my-4 text-justify">
+                    <div className="my-4 text-justify break-words">
                         {third.image && (<div className="image-cover mb-4 overflow-hidden"><img src={third.image} /></div>)}
-                        {third.text.replace(/\n/g, "\n\n")}
+                        {third.text.forDisplay()}
                     </div>
                 </>}
             </div>
@@ -169,21 +173,21 @@ const QuadLayout = (props) => {
         <div className="flex flex-row three-items border-b border-gray-500 py-5">
             <div className="eight-columns">
                 <h4 className="my-2">{first.title}</h4>
-                <div className="my-4 text-justify">{first.text.replace(/\n/g, "\n\n")}</div>
+                <div className="my-4 text-justify">{first.text.forDisplay()}</div>
                 <h4 className="my-2">{second.title}</h4>
-                <div className="my-4 text-justify">{second.text.replace(/\n/g, "\n\n")}</div>
+                <div className="my-4 text-justify">{second.text.forDisplay()}</div>
                 <h4 className="my-2">{third.title}</h4>
-                <div className="my-4 text-justify">{third.text.replace(/\n/g, "\n\n")}</div>
+                <div className="my-4 text-justify">{third.text.forDisplay()}</div>
                 <h4 className="my-2">{fourth.title}</h4>
-                <div className="my-4 text-justify">{fourth.text.replace(/\n/g, "\n\n")}</div>
+                <div className="my-4 text-justify">{fourth.text.forDisplay()}</div>
             </div>
         </div>
     );
 };
 
 const App = () => {
-    const [ data, setData ] = useState({ articles: [] });
-    const [ topic, setTopic ] = useState('everything');
+    const [data, setData] = useState({ articles: [] });
+    const [topic, setTopic] = useState('everything');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -201,41 +205,48 @@ const App = () => {
 
     const Articles = () => {
         let groups = [];
-        while (data.articles.length > 3) {
-            groups.push(data.articles.take(3));
+        let len = 0;
+        const totalLen = data.articles.reduce((sum, a) => sum + a.text.length, 0);
+        console.log("DBG::TOAL", totalLen);
+        let grabbedLen = 0;
+        while (totalLen - grabbedLen > 7000) {
+            len = 0;
+            let gr = [];
+            while (len < 7000) {
+                const article = data.articles.shift();
+                if (!article) break;
+                let l = article.text.length;
+                len += l;
+                gr.push(article);
+            }
+            grabbedLen += len
+            console.log("DBG::GRABBED", grabbedLen);
+            groups.push(gr);
         }
-        if (data.articles.length) groups.push(data.articles);
-        return groups.length && groups.map((g, i) => <TripleLayout key={i} articles={g}/>);
+        // if (data.articles.length) groups.push(data.articles);
+        return groups.length && groups.map((g, i) => <TripleLayout key={i} articles={g} />);
     };
 
     return (
-    <div className="w-full h-auto">
-        <div className="header w-full p-5 font-serif">
-            <div className="text-6xl font-bold mx-auto text-center" style={{ fontVariant: "small-caps" }}>The Large Print</div>
-    <div className="text-base mx-auto text-center uppercase py-2 border-b-2 border-t-2 border-black" style={{ letterSpacing: "5px" }}>{moment(new Date()).format("dddd, MMMM Do YYYY")} | {data.articles.length} articles</div>
-            {/* <div className="w-1/2 mx-auto">
-                <ul className="flex font-sans">
-                    <li className={`px-4 py-2 m-2 rounded-full border-2 nav-article-topic ${topic == 'everything' ? 'active' : ''}`}><a onClick={() => {setTopic('everything')}}>Everything</a></li>
-                    <li className={`px-4 py-2 m-2 rounded-full border-2 nav-article-topic ${topic == 'vietnamese' ? 'active' : ''}`}><a onClick={() => {setTopic('vietnamese')}}>Vietnamese</a></li>
-                    <li className={`px-4 py-2 m-2 rounded-full border-2 nav-article-topic ${topic == 'financial' ? 'active' : ''}`}><a onClick={() => {setTopic('financial')}}>Financial</a></li>
-                    <li className={`px-4 py-2 m-2 rounded-full border-2 nav-article-topic ${topic == 'other' ? 'active' : ''}`}><a onClick={() => {setTopic('other')}}>Others</a></li>
-                </ul>
-            </div> */}
-        </div>
-        <div className="w-full font-serif text-base">
-            <div className="flex-1 flex flex-col mx-auto p-5">
-                {!data.articles.length ? (<div className="h-screen content-center">Nothing here, please get back later!</div>) : ("")}
-                {/* {data.articles.length && <>
-                    <FullPageLayout articles={{one, four}} />
-                    <QuadLayout articles={four} />
-                    <FullPageLayoutLeft articles={{ one, four }} />
-                    <TripleLayout articles={three} />
-                </>} */}
-                <Articles/>
+        <div className="w-full h-auto">
+            <div className="header w-full p-5 pb-0 font-serif">
+                <div className="text-6xl font-bold mx-auto text-center" style={{ fontVariant: "small-caps" }}>The Large Print</div>
+                <div className="text-base mx-auto text-center uppercase py-2 border-b-2 border-t-2 border-black" style={{ letterSpacing: "5px" }}>{moment(new Date()).format("dddd, MMMM Do YYYY")} | {data.articles.length} articles</div>
+                <div className="text-base mx-auto text-center uppercase pt-4">
+                    <div className={`inline-block mx-5 ${topic == 'everything' ? 'underline' : ''}`}><a onClick={() => { setTopic('everything') }}>Everything</a></div>
+                    <div className={`inline-block mx-5 ${topic == 'vietnamese' ? 'underline' : ''}`}><a onClick={() => { setTopic('vietnamese') }}>Vietnamese</a></div>
+                    <div className={`inline-block mx-5 ${topic == 'financial' ? 'underline' : ''}`}><a onClick={() => { setTopic('financial') }}>Financial</a></div>
+                    <div className={`inline-block mx-5 ${topic == 'other' ? 'underline' : ''}`}><a onClick={() => { setTopic('other') }}>Others</a></div>
+                </div>
+            </div>
+            <div className="w-full font-serif text-base">
+                <div className="flex-1 flex flex-col mx-auto p-5">
+                    {!data.articles.length ? (<div className="h-screen content-center">Nothing here, please get back later!</div>) : ("")}
+                    <Articles />
+                </div>
             </div>
         </div>
-    </div>
     )
 };
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
